d0874337b912f0d04175d069975abbe15e29dc7e
YuriBocharov <quesadillaman@gmail.com>
2023-11-24 19:15:15 -0500

    basic rest server

--- server/main.go
+++ server/main.go
@@ -14,6 +14,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+
 	"github.com/gorilla/websocket"
 )
 
@@ -43,16 +44,48 @@ func getAlbumsApi(w http.ResponseWriter, _ *http.Request) {
 	w.Write(bytes)
 }
 
-func postAlbums(w http.ResponseWriter, r *http.Request) {
+func postAlbumsAPI(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	var newAlbum database.Album
 	err := json.NewDecoder(r.Body).Decode(&newAlbum)
 	if err != nil {
 		log.Printf("Error decoding album: %v", err)
+		http.Redirect(w, r, "/albums/new", http.StatusSeeOther)
 	}
 
-	albums.Add(newAlbum)
+	id, err := albums.Add(newAlbum)
+	if err != nil {
+		log.Printf("Error adding album: %v", err)
+		http.Redirect(w, r, "/albums/new", http.StatusSeeOther)
+	}
+
+	http.Redirect(w, r, fmt.Sprintf("/albums/%d", id), http.StatusSeeOther)
+}
+
+func postAlbum(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var newAlbum database.Album
+	newAlbum.Title = template.HTMLEscapeString(r.FormValue("title"))
+	newAlbum.Artist = template.HTMLEscapeString(r.FormValue("artist"))
+
+	price := r.FormValue("price")
+	val, err := strconv.ParseFloat(price, 64)
+	if err != nil {
+		log.Printf("Error decoding album: %v", err)
+		http.Redirect(w, r, "/albums/new", http.StatusSeeOther)
+	}
+
+	newAlbum.Price = float32(val)
+
+	id, err := albums.Add(newAlbum)
+	if err != nil {
+		log.Printf("Error adding album: %v", err)
+		http.Redirect(w, r, "/albums/new", http.StatusSeeOther)
+	}
+
+	http.Redirect(w, r, fmt.Sprintf("/albums/%d", id), http.StatusSeeOther)
 }
 
 func deleteAlbum(w http.ResponseWriter, r *http.Request) {
@@ -72,10 +105,16 @@ func getAlbum(w http.ResponseWriter, r *http.Request) {
 	albumID := chi.URLParam(r, "albumID")
 
 	id, err := strconv.Atoi(albumID)
-	catch(err)
+	if err != nil {
+		http.Redirect(w, r, "/albums", http.StatusSeeOther)
+		catch(err)
+	}
 
 	album, err := albums.AlbumByID(id)
-	catch(err)
+	if err != nil {
+		http.Redirect(w, r, "/albums", http.StatusSeeOther)
+		catch(err)
+	}
 
 	t, err := template.ParseFiles("templates/base.html", "templates/album.html")
 	catch(err)
@@ -120,8 +159,9 @@ func getAlbumApi(w http.ResponseWriter, r *http.Request) {
 	w.Write(bytes)
 }
 
-func showIndex(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome home!")
+func showIndex(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.Write([]byte("Hello World!"))
 }
 
 func main() {
@@ -152,15 +192,52 @@ func main() {
 	r.Get("/livereload.js", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "./js/livereload.js")
 	})
+
 	r.Get("/", showIndex)
-	r.Get("/albums", getAlbums)
 	r.Get("/alive", serverAlive)
+	r.Route("/albums", func(r chi.Router) {
+		r.Get("/", getAlbums)
+		r.Get("/new", newAlbum)
+		r.Get("/{albumID}/edit", editAlbum)
+		r.Post("/", postAlbum)
+		r.Get("/{albumID}", getAlbum)
+		r.Delete("/{albumID}", deleteAlbum)
+	})
 	// r.Get("/albums", getAlbumsApi)
-	r.Get("/albums/{albumID}", getAlbum)
-	r.Delete("/albums/{albumID}", deleteAlbum)
-	r.Post("/albums", postAlbums)
 
 	err = http.ListenAndServe(":8080", r)
+	catch(err)
+}
+
+func editAlbum(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+
+	w.Header().Set("Content-Type", "text/html")
+	albumID := chi.URLParam(r, "albumID")
+
+	id, err := strconv.Atoi(albumID)
+	if err != nil {
+		http.Redirect(w, r, "/albums", http.StatusSeeOther)
+		catch(err)
+	}
+
+	album, err := albums.AlbumByID(id)
+	if err != nil {
+		http.Redirect(w, r, "/albums", http.StatusSeeOther)
+		catch(err)
+	}
+
+	t, err := template.ParseFiles("templates/base.html", "templates/albums/new.html", "templates/albums/form.html")
+	catch(err)
+	err = t.Execute(w, album)
+	catch(err)
+}
+
+func newAlbum(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	t, err := template.ParseFiles("templates/base.html", "templates/albums/new.html", "templates/albums/form.html")
+	catch(err)
+	err = t.Execute(w, nil)
 	catch(err)
 }
 
